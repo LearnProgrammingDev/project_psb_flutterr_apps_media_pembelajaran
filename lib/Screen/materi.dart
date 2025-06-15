9e13676c1536adb4efc60a9ba414910a74b73673
Randz Code <learnprogramming65@gmail.com>
2025-06-16 02:56:55 +0700

    membuat fitur profile lebih menarik

---
+++ lib/Screen/materi.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Materi extends StatelessWidget {
+  const Materi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: const Center(child: Text('materi')),
+    );
+  }
+}
---
+++ lib/navigation.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:project_splashscreen/Screen/materi.dart';
+import 'package:project_splashscreen/Screen/bantuan.dart';
+import 'package:project_splashscreen/Screen/profile.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  List screens = [const Materi(), const Bantuan(), const Profile()];
+
+  int _intpage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        index: _intpage,
+        backgroundColor: Colors.blueAccent,
+        items: <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.compare_arrows, size: 30),
+        ],
+
+        onTap: (index) {
+          //Handle button tap
+          setState(() {
+            _intpage = index;
+          });
+        },
+      ),
+      body: screens[_intpage],
+    );
+  }
+}
